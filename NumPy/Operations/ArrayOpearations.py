de0f2c33b76f13d9bd38ef32c7c5b99ae6c26991
dileep-mallem <mailtodileepmallem@gmail.com>
2026-08-20 20:55:32 +0530

    Arithmetic op's , Scalar Broadcasting , Comparion , dot and matrix muliply

---
+++ NumPy/Operations/ArrayOpearations.py
@@ -0,0 +1,28 @@
+import numpy as np 
+
+a=np.array([1,2,3,4])
+b=np.array([10,20,30,40])
+
+# Element Wise Arithmetic No Loops Needed (Shapes Should be According to Requirement of Opearion)
+
+print(a+b)
+print(b-a)
+print(a*b)
+print(b//a)
+print(a**2)
+print(b%10)
+print("\nScalar Broadcasting \n")
+# Scalar Broadcasting -> Scalar applied to every Element 
+print(a*5)
+print(a+10)
+
+# Comaparision -> returns Boolean Array 
+print(a>2)
+print(a==b//10)
+
+# Dot product and Matrix Multiplication 
+print(np.dot(a,b)) # 300  (1*10 + 2*20 + 3*30 + 4*40)
+m1=np.array([[1,2],[3,4]])
+m2=np.array([[5,6],[7,8]])
+print(m1 @ m2) # matrix multiplication (@ operator)
+
